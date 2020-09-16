fb80d157a09d858b4f19d1227e35d48ce49b88a3
Pavelescu Victor <pavelescu.victor97@gmail.com>
2020-09-16 17:57:45 +0300

    Texture optimization for faster loadtime.

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,17 @@
     <div id="app">
         <div id="app__content">
             <div class="navbar__wrapper">
-                <Navbar @updatePage="changeDisplayedPage"/>
+                <Navbar @updatePage="changeDisplayedPage" />
             </div>
             <div class="content__wrapper">
                 <div class="main" v-bind:class="{notDisplayedPage : !isMainActive}">
                     <Main />
                 </div>
                 <div class="map" v-bind:class="{notDisplayedPage : !isMapActive}">
-                    <Map :isMapActive="isMapActive"/>
+                    <Map :isMapActive="isMapActive" />
                 </div>
                 <div class="globe" v-bind:class="{notDisplayedPage : !isGlobeActive}">
-                    <Globe :isGlobeActive="isGlobeActive"/>
+                    <Globe :isGlobeActive="isGlobeActive" />
                 </div>
             </div>
         </div>
@@ -20,188 +20,183 @@
 </template>
 
 <script>
-    import Map from "./components/Map.vue";
-    import Navbar from "./components/Navbar.vue";
-    import Globe from "./components/Globe.vue";
-    import Main from "./components/Main.vue";
-    export default {
-        name: "App",
-        components: {
-            Map,
-            Navbar,
-            Globe,
-            Main,
+import Map from "./components/Map.vue";
+import Navbar from "./components/Navbar.vue";
+import Globe from "./components/Globe.vue";
+import Main from "./components/Main.vue";
+export default {
+    name: "App",
+    components: {
+        Map,
+        Navbar,
+        Globe,
+        Main,
+    },
+    data() {
+        return {
+            showedPage: "main",
+            isMainActive: true,
+            isMapActive: false,
+            isGlobeActive: false,
+        };
+    },
+    methods: {
+        changeDisplayedPage(e) {
+            this.showedPage = e;
         },
-        data() {
-            return {
-                showedPage: 'main',
-                isMainActive: true,
-                isMapActive: false,
-                isGlobeActive: false,
-            };
-        },
-        methods: {
-            changeDisplayedPage(e) {
-                this.showedPage = e;
-            },
-        },
-        watch: {
-            showedPage: function(val) {
-                if(val === "main") {
-                    this.isMainActive = true;
-                    this.isMapActive = false;
-                    this.isGlobeActive = false;
-                }
-                else if(val === "map") {
-                    this.isMainActive = false;
-                    this.isMapActive = true;
-                    this.isGlobeActive = false;
-                }
-                else if(val === "globe") {
-                    this.isMainActive = false;
-                    this.isMapActive = false;
-                    this.isGlobeActive = true;
-                }
+    },
+    watch: {
+        showedPage: function (val) {
+            if (val === "main") {
+                this.isMainActive = true;
+                this.isMapActive = false;
+                this.isGlobeActive = false;
+            } else if (val === "map") {
+                this.isMainActive = false;
+                this.isMapActive = true;
+                this.isGlobeActive = false;
+            } else if (val === "globe") {
+                this.isMainActive = false;
+                this.isMapActive = false;
+                this.isGlobeActive = true;
             }
-        }
-    };
+        },
+    },
+};
 </script>
 
 <style>
-    /* IMPORTING FONTS */
+/* IMPORTING FONTS */
 
-    @import url('https://fonts.googleapis.com/css2?family=Fjalla+One&family=Pacifico&display=swap');
-    @import url('https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap');
-    @import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@700&display=swap');
-    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Fjalla+One&family=Pacifico&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@700&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap");
 
-    :root{
-        /* COLOR VARIABLES */
+:root {
+    /* COLOR VARIABLES */
 
-        --color-green: #00B074;
-        --color-lightgreen: #D1F1E6;
-        --color-lightgrey: #EDF0EE;
-        --color-white: #FFFFFF;
-        --color-lightblue: #BEDEDE;
-        --color-darkblue: #2B3940;
-        --color-grey: #7E8989;
+    --color-green: #00b074;
+    --color-lightgreen: #d1f1e6;
+    --color-lightgrey: #edf0ee;
+    --color-white: #ffffff;
+    --color-lightblue: #bedede;
+    --color-darkblue: #2b3940;
+    --color-grey: #7e8989;
 
-    
-        --color-blue: #4C84FF;
-        --color-lightgrey-2: #E7E9EE;
-        --color-lightgrey-3: #F5F6FA;
-        --color-darkblue-2: #454D61;
-        --color-green-2: #40CB9E;
-        --color-yellow: #F9C108;
-        --color-lightyellow: #FFF4CC;
+    --color-blue: #4c84ff;
+    --color-lightgrey-2: #e7e9ee;
+    --color-lightgrey-3: #f5f6fa;
+    --color-darkblue-2: #454d61;
+    --color-green-2: #40cb9e;
+    --color-yellow: #f9c108;
+    --color-lightyellow: #fff4cc;
 
-        /* TEXT */
+    /* TEXT */
 
-        --textBaseSize: 16px;
+    --textBaseSize: 16px;
 
-        /* FONT VARIABLES */
+    /* FONT VARIABLES */
 
-        --website-font: 'Fjalla One', sans-serif;
-        --logo-font: 'Fredoka One', cursive;;
-        --nav-font: 'Roboto Condensed', sans-serif;
-        /*--nav-font: 'Montserrat', sans-serif;*/
-        --font-family-base: Arial, Helvetica, sans-serif;
+    --website-font: "Fjalla One", sans-serif;
+    --logo-font: "Fredoka One", cursive;
+    --nav-font: "Roboto Condensed", sans-serif;
+    /*--nav-font: 'Montserrat', sans-serif;*/
+    --font-family-base: Arial, Helvetica, sans-serif;
 
-        
-        /* MARGINS */
+    /* MARGINS */
 
-        --margin-1: 60px;    
-        --margin-2: 30px;
+    --margin-1: 60px;
+    --margin-2: 30px;
 
-        /* PADDINGS */
+    /* PADDINGS */
 
-        --padding-1: 60px;    
-        --padding-2: 30px;     
-        
-        /* BORDER */
-        
-        --border-radius-1: 15px;
-        --border-radius-2: 50px;
+    --padding-1: 60px;
+    --padding-2: 30px;
 
-    }
+    /* BORDER */
 
-    * {
-        margin: 0px;
-        padding: 0px;
-        box-sizing: border-box;
-    }
+    --border-radius-1: 15px;
+    --border-radius-2: 50px;
+}
 
-    body, html {
-        scroll-behavior: smooth;
-        width: 100%;
-        height: 100%;
-        min-height: 100vh;
-        background: var(--color-lightgrey-2);
-        font-family: var(--nav-font);
-        color: var(--color-darkblue-2);
-        font-size: var(--text-base-size);
-    }
+* {
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+}
 
-    a {
-        text-decoration: none;
-    }
+body,
+html {
+    scroll-behavior: smooth;
+    width: 100%;
+    height: 100%;
+    min-height: 100vh;
+    background: var(--color-lightgrey-2);
+    font-family: var(--nav-font);
+    color: var(--color-darkblue-2);
+    font-size: var(--text-base-size);
+}
 
-    h1 {
-        height: 10vh;
-        line-height: 10vh;
-    }
+a {
+    text-decoration: none;
+}
 
-    #app {
-        height: 100%;
-        width: 100%;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-    }
+h1 {
+    height: 10vh;
+    line-height: 10vh;
+}
 
-    #app__content {
-        min-height: 100vh;
-        background: var(--color-lightgrey-2);
-        padding: var(--padding-1);
-        display: grid;
-        grid-template-rows: auto 1fr;
-    }
+#app {
+    height: 100%;
+    width: 100%;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+}
 
-    .navbar__wrapper {
-        border-top-left-radius: var(--border-radius-1);
-        border-top-right-radius: var(--border-radius-1);
-        overflow: hidden;
-    }
+#app__content {
+    min-height: 100vh;
+    background: var(--color-lightgrey-2);
+    padding: var(--padding-1);
+    display: grid;
+    grid-template-rows: auto 1fr;
+}
 
-    .content__wrapper {
-        border-bottom-left-radius: var(--border-radius-1);
-        border-bottom-right-radius: var(--border-radius-1);
-        overflow: hidden;
-    }
+.navbar__wrapper {
+    border-top-left-radius: var(--border-radius-1);
+    border-top-right-radius: var(--border-radius-1);
+    overflow: hidden;
+}
 
-    .map{
-        height: 100%;
-        background: var(--color-lightgrey-3);
-    }
+.content__wrapper {
+    border-bottom-left-radius: var(--border-radius-1);
+    border-bottom-right-radius: var(--border-radius-1);
+    overflow: hidden;
+}
 
-    /* Main */
+.map {
+    height: 100%;
+    background: var(--color-lightgrey-3);
+}
 
-    .main{
-        height: 100%;
-        background: var(--color-lightgrey-3);
-    }
+/* Main */
 
-    /* Globe */
+.main {
+    height: 100%;
+    background: var(--color-lightgrey-3);
+}
 
-    .globe{
-        height: 100%;
-        background: var(--color-lightgrey-3);
-    }
+/* Globe */
 
-    /* UTILITY CLASSES */
+.globe {
+    height: 100%;
+    background: var(--color-lightgrey-3);
+}
 
-    .notDisplayedPage{
-        display: none;
-    }
+/* UTILITY CLASSES */
 
+.notDisplayedPage {
+    display: none;
+}
 </style>
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,7 +1,15 @@
 <template>
     <div id="map">
         <div class="map__wrapper">
-            <MglMap :accessToken="accessToken" :mapStyle="mapStyle" :center="coordinates" :zoom="zoom" :attributionControl="false">
+            <MglMap
+                :accessToken="accessToken"
+                :mapStyle="mapStyle"
+                :center="coordinates"
+                :zoom="zoom"
+                :attributionControl="false"
+                container="map__wrapper"
+                @load="mapLoaded"
+            >
                 <MglNavigationControl position="top-left" />
                 <MglGeolocateControl position="top-left" />
                 <MglFullscreenControl position="top-left" />
@@ -17,24 +25,48 @@
             <h1>{{ msg }}</h1>
             <div class="form__wrapper">
                 <form @submit.prevent="generateQuakesData">
-                    <div class="form__error" v-bind:class="{notDisplayed: formErrorNull}">
-                        <p v-text="formError" ></p>
+                    <div
+                        class="form__error"
+                        v-bind:class="{ notDisplayed: formErrorNull }"
+                    >
+                        <p v-text="formError"></p>
                     </div>
                     <div class="days">
                         <label>Enter number of days</label>
-                        <input type="number" id="days" name="days" v-model="days" max="30"/>
+                        <input
+                            type="number"
+                            id="days"
+                            name="days"
+                            v-model="days"
+                            max="30"
+                        />
                     </div>
                     <div class="depth-max">
                         <label>Maximum depth</label>
-                        <input type="number" id="maxDepth" name="maxDepth" v-model="maxDepth" />
+                        <input
+                            type="number"
+                            id="maxDepth"
+                            name="maxDepth"
+                            v-model="maxDepth"
+                        />
                     </div>
                     <div class="depth-min">
                         <label>Minimum depth</label>
-                        <input type="number" id="minDepth" name="minDepth" v-model="minDepth" />
+                        <input
+                            type="number"
+                            id="minDepth"
+                            name="minDepth"
+                            v-model="minDepth"
+                        />
                     </div>
                     <div class="magnitude-min">
                         <label for="magnitude">Minimum magnitude</label>
-                        <input type="number" id="quantity" name="quantity" v-model="minMagnitude" />
+                        <input
+                            type="number"
+                            id="quantity"
+                            name="quantity"
+                            v-model="minMagnitude"
+                        />
                     </div>
                     <div class="submit">
                         <input type="submit" id="submit" value="Submit" />
@@ -46,7 +78,7 @@
 </template>
 <script>
 import Mapbox from "mapbox-gl";
-import { 
+import {
     MglMap,
     MglNavigationControl,
     MglGeolocateControl,
@@ -54,38 +86,39 @@ import {
     MglGeojsonLayer,
 } from "vue-mapbox";
 // So that markes dont move weirdly on zoom
-import 'mapbox-gl/dist/mapbox-gl.css';
-import axios from 'axios';
+import "mapbox-gl/dist/mapbox-gl.css";
+import axios from "axios";
 export default {
     name: "Map",
     components: {
-        MglMap, 
+        MglMap,
         MglNavigationControl,
         MglGeolocateControl,
         MglFullscreenControl,
         MglGeojsonLayer,
     },
-    data () {
+    data() {
         return {
             msg: "Earthquakes Map Visualization",
             mapbox: null,
-            accessToken: 'pk.eyJ1IjoiMjA0M253IiwiYSI6ImNrZG9qeWhtMDBoMGYycXFvNm9nZnh4YzMifQ.djEvGtmsohtkGeFhV-0OFg',
-            mapStyle: 'mapbox://styles/mapbox/streets-v11',
+            accessToken:
+                "pk.eyJ1IjoiMjA0M253IiwiYSI6ImNrZG9qeWhtMDBoMGYycXFvNm9nZnh4YzMifQ.djEvGtmsohtkGeFhV-0OFg",
+            mapStyle: "mapbox://styles/mapbox/streets-v11",
             coordinates: [0, 0],
             zoom: 0,
-            days: '',
-            maxDepth: '',
-            minDepth: '',
-            minMagnitude: '',
-            formError: '',
+            days: "",
+            maxDepth: "",
+            minDepth: "",
+            minMagnitude: "",
+            formError: "",
             formErrorNull: true,
             formErrorTime: 2500,
             geoJsonSource: {
-                type: 'geojson',
+                type: "geojson",
                 data: {
                     id: "thisIsMySource",
                     type: "FeatureCollection",
-                    features: []
+                    features: [],
                 },
             },
             geoJsonLayer: {
@@ -93,161 +126,184 @@ export default {
                 paint: {
                     "circle-color": "#4C84FF",
                     "circle-opacity": 0.5,
-                    'circle-radius': {
-                        'base': 10,
-                        'stops': [
-                        [12, 10],
-                        [22, 180]
-                        ]
+                    "circle-radius": {
+                        base: 10,
+                        stops: [
+                            [12, 10],
+                            [22, 180],
+                        ],
                     },
-                }
+                },
             },
-        }
+        };
     },
     methods: {
         generateQuakesData() {
-            this.formError = '';
+            this.formError = "";
             let daysSetFlag = true;
             let maxDepthSetFlag = true;
             let minDepthSetFlag = true;
             let minMagnitudeSetFlag = true;
 
-            if(this.days === '') daysSetFlag = false;
-            if(this.maxDepth === '') maxDepthSetFlag = false;
-            if(this.minDepth === '') minDepthSetFlag = false;
-            if(this.minMagnitude === '') minMagnitudeSetFlag = false;
-            
-            if(daysSetFlag === false) this.formError = "The day counter must be set!";
-            else if(maxDepthSetFlag === false) this.formError = "The max depth counter must be set!";
-            else if(minDepthSetFlag === false) this.formError = "The min depth counter must be set!";
-            else if(minMagnitudeSetFlag === false) this.formError = "The min magnitude counter must be set";
-            else{
+            if (this.days === "") daysSetFlag = false;
+            if (this.maxDepth === "") maxDepthSetFlag = false;
+            if (this.minDepth === "") minDepthSetFlag = false;
+            if (this.minMagnitude === "") minMagnitudeSetFlag = false;
+
+            if (daysSetFlag === false)
+                this.formError = "The day counter must be set!";
+            else if (maxDepthSetFlag === false)
+                this.formError = "The max depth counter must be set!";
+            else if (minDepthSetFlag === false)
+                this.formError = "The min depth counter must be set!";
+            else if (minMagnitudeSetFlag === false)
+                this.formError = "The min magnitude counter must be set";
+            else {
                 var currentdate = new Date();
                 var queryDate = new Date();
                 queryDate.setDate(currentdate.getDate() - this.days);
-                var dateTime = `${currentdate.getFullYear()}-${(currentdate.getMonth()+1)}-${currentdate.getDate()}T${currentdate.getHours()}:${currentdate.getMinutes()}:${currentdate.getSeconds()}`
-                var queryDateTime = `${queryDate.getFullYear()}-${(queryDate.getMonth()+1)}-${queryDate.getDate()}T${queryDate.getHours()}:${queryDate.getMinutes()}:${queryDate.getSeconds()}`
-                axios.get(`https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=${queryDateTime}&endtime=${dateTime}&minmagnitude=${this.minMagnitude}&maxdepth=${this.maxDepth}&mindepth=${this.minDepth}`).then(response => {
-                    this.geoJsonSource = response;
-                    this.geoJsonSource.data['id'] = "thisIsMySource";
-                });
+                var dateTime = `${currentdate.getFullYear()}-${currentdate.getMonth() +
+                    1}-${currentdate.getDate()}T${currentdate.getHours()}:${currentdate.getMinutes()}:${currentdate.getSeconds()}`;
+                var queryDateTime = `${queryDate.getFullYear()}-${queryDate.getMonth() +
+                    1}-${queryDate.getDate()}T${queryDate.getHours()}:${queryDate.getMinutes()}:${queryDate.getSeconds()}`;
+                axios
+                    .get(
+                        `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=${queryDateTime}&endtime=${dateTime}&minmagnitude=${this.minMagnitude}&maxdepth=${this.maxDepth}&mindepth=${this.minDepth}`
+                    )
+                    .then((response) => {
+                        this.geoJsonSource = response;
+                        this.geoJsonSource.data["id"] = "thisIsMySource";
+                    });
             }
         },
         removeFormErrorText() {
             this.formError = "";
         },
         mapLoaded(e) {
-            this.mapbox = e.Mapbox;
-        }
+            this.mapbox = e.mapbox;
+        },
     },
-    created(){
+    created() {
         this.mapbox = Mapbox;
     },
-   watch: {
-       geoJsonSource: function (val){
-           console.log(val);
-       },
-       formError: function (val){
-           if(val === "") this.formErrorNull = true;
-           else {
-               setTimeout(this.removeFormErrorText, this.formErrorTime);
-               this.formErrorNull = false;
-           }
-       }
-   }
-}
+    watch: {
+        geoJsonSource: function(val) {
+            console.log(val);
+        },
+        formError: function(val) {
+            if (val === "") this.formErrorNull = true;
+            else {
+                setTimeout(this.removeFormErrorText, this.formErrorTime);
+                this.formErrorNull = false;
+            }
+        },
+    },
+};
 </script>
 <style scoped>
-    #map {
-        height: 100%;
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-    }
+#map {
+    height: 100%;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+}
 
-    .map__wrapper{
-        height: 100%;
-    }
+.map__wrapper {
+    height: 100%;
+}
 
-    .map__content{
-        display: grid;
-        grid-template-rows: auto 1fr;
-    }
+.map__content {
+    display: grid;
+    grid-template-rows: auto 1fr;
+}
 
-    .map__content h1 {
-        padding: calc(var(--padding-2) / 2);
-        font-size: 2rem;
-    }
+.map__content h1 {
+    padding: calc(var(--padding-2) / 2);
+    font-size: 2rem;
+}
 
-    .form__wrapper {
-        display: flex;
-        flex-direction: column;
-    }
+.form__wrapper {
+    display: flex;
+    flex-direction: column;
+}
 
-    .form__error p{
-        height: var(--padding-1);
-        transform: translateX(-50%);
-        position: absolute;
-        top: 0px;
-        line-height: var(--padding-1);
-        font-size: 1.4rem;
-        background: var(--color-blue);
-        padding: 0px var(--padding-1);
-        color: #FFFFFF;
-        border-bottom-left-radius: 50px;
-        border-bottom-right-radius: 50px;
-    }
-    
-    .days, .magnitude-min, .depth-min, .depth-max {
-        padding: calc(var(--padding-2) / 2);
-        display: grid;
-        align-items: left;
-    }
+.form__error p {
+    height: var(--padding-1);
+    transform: translateX(-50%);
+    position: absolute;
+    top: 0px;
+    line-height: var(--padding-1);
+    font-size: 1.4rem;
+    background: var(--color-blue);
+    padding: 0px var(--padding-1);
+    color: #ffffff;
+    border-bottom-left-radius: 50px;
+    border-bottom-right-radius: 50px;
+}
 
-    .days label, .magnitude-min label, .depth-min label, .depth-max label {
-        line-height: 1.3rem;
-        font-size: 1.3rem;
-        padding: 10px;
-        margin: calc(var(--padding-2) / 4);
-    }
+.days,
+.magnitude-min,
+.depth-min,
+.depth-max {
+    padding: calc(var(--padding-2) / 2);
+    display: grid;
+    align-items: left;
+}
 
-    .days input, .magnitude-min input, .depth-min input, .depth-max input {
-        border:none;
-        outline:none;
-        appearance: none;
-        height: calc(1.5 * var(--padding-2));
-        border-radius:22px;
-        box-sizing:border-box;
-        padding: 0 var(--padding-2);
-        margin: 0 var(--padding-1);
-        transition: margin .2s ease-in;
-    }
-    
-    .days input:hover, .magnitude-min input:hover, .depth-min input:hover, .depth-max input:hover {
-        margin: 0 var(--padding-2);
-    }
+.days label,
+.magnitude-min label,
+.depth-min label,
+.depth-max label {
+    line-height: 1.3rem;
+    font-size: 1.3rem;
+    padding: 10px;
+    margin: calc(var(--padding-2) / 4);
+}
 
-    .submit {
-        padding: calc(var(--padding-2) / 2);
-    }
+.days input,
+.magnitude-min input,
+.depth-min input,
+.depth-max input {
+    border: none;
+    outline: none;
+    appearance: none;
+    height: calc(1.5 * var(--padding-2));
+    border-radius: 22px;
+    box-sizing: border-box;
+    padding: 0 var(--padding-2);
+    margin: 0 var(--padding-1);
+    transition: margin 0.2s ease-in;
+}
 
-    .submit input {
-        padding: calc(var(--padding-2) / 2);
-        border: none;
-        background: #FFFFFF;
-        cursor: pointer;
-        transition: padding .2s ease-in, background .2s ease-in, color .2s ease-in, border-radius .2s ease-in;
-    }
+.days input:hover,
+.magnitude-min input:hover,
+.depth-min input:hover,
+.depth-max input:hover {
+    margin: 0 var(--padding-2);
+}
 
-    .submit input:hover {
-        background: var(--color-darkblue);
-        color: var(--color-lightgrey);
-        padding: calc(var(--padding-2) / 2) var(--padding-2);
-        border-radius: var(--border-radius-2);
-    }
+.submit {
+    padding: calc(var(--padding-2) / 2);
+}
 
-    /* UTILITY CLASSES */
+.submit input {
+    padding: calc(var(--padding-2) / 2);
+    border: none;
+    background: #ffffff;
+    cursor: pointer;
+    transition: padding 0.2s ease-in, background 0.2s ease-in,
+        color 0.2s ease-in, border-radius 0.2s ease-in;
+}
 
-    .notDisplayed {
-        display: none;
-    }
+.submit input:hover {
+    background: var(--color-darkblue);
+    color: var(--color-lightgrey);
+    padding: calc(var(--padding-2) / 2) var(--padding-2);
+    border-radius: var(--border-radius-2);
+}
+
+/* UTILITY CLASSES */
+
+.notDisplayed {
+    display: none;
+}
 </style>
